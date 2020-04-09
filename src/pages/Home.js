678a993d950b73368a5903f32878feba69507ed8
Jonasz Łaba <jonasz.laba@clebre.com>
2020-04-09 19:02:32 +0200

    asd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,17 @@
-import React from "react";
-import CocktailList from "../components/CocktailList";
-import SearchForm from "../components/SearchForm";
+import React from 'react';
+import CocktailList from '../components/CocktailList';
+import SearchForm from '../components/SearchForm';
 
 export default function Home() {
   const [loading, setLoading] = React.useState(false);
-  const [searchTerm, setSearchTerm] = React.useState("a");
+  const [searchTerm, setSearchTerm] = React.useState('a');
   const [cocktails, setCocktails] = React.useState([]);
 
   React.useEffect(() => {
-    fetch("https://ww.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
-      .then((response) => {
-        response.json();
-      })
+    fetch(
+      'https://ww.thecocktaildb.com/api/json/v1/1/search.php?s=margarita',
+    )
+      .then((response) => response.json())
       .then((data) => console.log(data));
   }, []);
   return (
