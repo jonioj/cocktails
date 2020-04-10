3250c44c3d759acd8ea612c36b13852c84ae82e3
Jonasz Łaba <jonasz.laba@clebre.com>
2020-04-10 11:27:00 +0200

    async

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,16 +5,27 @@ import axios from 'axios';
 
 export default function Home() {
   const [loading, setLoading] = React.useState(false);
-  const [searchTerm, setSearchTerm] = React.useState('a');
+  const [searchTerm, setSearchTerm] = React.useState('margarita');
   const [cocktails, setCocktails] = React.useState([]);
 
   React.useEffect(() => {
-    fetch(
-      'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita',
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }, []);
+    async function getDrinks() {
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`,
+        );
+        const data = await response.json();
+        const { drinks } = data;
+        if (drinks) {
+          console.log('drink');
+        } else {
+          console.log('no drinks');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [searchTerm]);
   return (
     <main>
       <SearchForm setSearchTerm={setSearchTerm} />
